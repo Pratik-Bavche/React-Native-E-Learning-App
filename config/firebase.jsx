cb1436f0e66e2bb9b43412b6a97606f6b2ccbf2e
Pratik <pratikbavche2005@gmail.com>
2025-11-18 18:27:00 +0530

    changes in db file

--- config/firebase.jsx
+++ config/firebase.jsx
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeAuth,getReactNativePersistence } from "firebase/auth";
+import { ReactNativeAsyncStorage} from "@react-native-async-storage/async-storage";
+import React from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +21,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 const analytics = getAnalytics(app);
