0afaa5c7701a2e3ec47070f2333b0a5555c32e30
Pratik <pratikbavche2005@gmail.com>
2025-11-18 12:28:50 +0530

    Home Page done

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,68 @@
-import { Image, Text, View } from "react-native";
-
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import Colors from "./../constant/Colors.jsx"
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-      }}
-    >
-     <Image source={require('./../assets/images/landing.png')}/> 
+        backgroundColor:Colors.WHITE,
+      }}>
+
+      
+     <Image source={require('./../assets/images/landing.png')}
+      style={{
+        width:'100%', 
+        height:450,
+        marginTop:70,
+        resizeMode:'contain',
+      }}/> 
+
+      <View style={{
+        padding:25,
+        backgroundColor:Colors.PRIMARY,
+        height:'100%',
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30
+      }}>
+
+        <Text style={{
+          fontSize:30,
+          fontWeight:"bold",
+          textAlign:"center",
+          color:Colors.WHITE
+        }}>Welcome to Future Classes</Text>
+
+
+        <Text style={{
+          fontSize:20,
+          color:Colors.WHITE,
+          marginTop:20,
+          textAlign:"center"
+        }}>Your ideas, transformed into meaningful learning experiences.</Text>
+
+        <View style={styles.button}>
+          <Text style={[styles.buttonText,{color:Colors.PRIMARY}]}>Sign In</Text>
+        </View>
+
+        <View style={[styles.button,{backgroundColor:Colors.PRIMARY,borderWidth:1,borderColor:Colors.WHITE}]}>
+          <Text style={[styles.buttonText,{color:Colors.WHITE}]}>Sign Up</Text>
+        </View>
+
+      </View>
     </View>
   );
 }
+
+//rns - react native styles
+const styles = StyleSheet.create({
+    button:{
+      padding:15,
+      backgroundColor:Colors.WHITE,
+      marginTop:30,
+      borderRadius:10
+    },
+    buttonText:{
+      textAlign:'center',
+      fontSize:18
+    }
+})
---
+++ constant/Colors.jsx
@@ -0,0 +1,5 @@
+
+export default{
+    WHITE:'#fff',
+    PRIMARY:'#e18649ff'
+}
