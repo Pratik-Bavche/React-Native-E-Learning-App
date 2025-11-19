252484565a0442f030076e8bc3369e6f70a9e791
Pratik <pratikbavche2005@gmail.com>
2025-11-19 13:57:07 +0530

    Home section done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,13 @@
 import { View, Text, Platform } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
-
+import Colors from '../../constant/Colors.jsx'
 export default function Home() {
   return (
-    <View style={{padding:25,paddingTop:Platform.OS=='ios'&&45}} >
+    <View style={{padding:25,paddingTop:Platform.OS=='ios'&&45,
+      flex:1, 
+      backgroundColor:Colors.WHITE
+    }} >
       <Header/>
     </View>
   )
--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import { UserDetailContext } from '../../context/UserDetailContext'
+import Ionicons from '@expo/vector-icons/Ionicons';
 export default function Header() {
+  const {userDetail,setUserDetail}=React.useContext(UserDetailContext)
   return (
-    <View>
-      <Text>Header</Text>
+    <View style={{flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+    }}>
+      <View>
+        <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize:25,
+        marginTop:30
+      }}>Welcome {userDetail?.fullName}</Text>
+      <Text style={{fontFamily:'outfit',fontSize:18}}>Let's Get Started!</Text>
+      </View>
+      <View>
+        <TouchableOpacity>
+          <Ionicons name="settings-outline" size={30} color="black" />
+        </TouchableOpacity>        
+      </View>
     </View>
   )
 }
