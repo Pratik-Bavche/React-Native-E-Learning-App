4b1b9b00b16cf6a8dcb455e4789bfd568139ec7e
Pratik <pratikbavche2005@gmail.com>
2025-11-23 17:43:06 +0530

    Practice section done

--- app/practice/[type]/index.jsx
+++ app/practice/[type]/index.jsx
@@ -1,21 +1,58 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import React from 'react';
+import { View, Text, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { imageAssets, PraticeOption } from '../../../constant/Option';
 import Colors from '../../../constant/Colors';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { getDocs, query, where, collection } from 'firebase/firestore';
+import { db } from './../../../config/firebase';
+import { UserDetailContext } from '../../../context/UserDetailContext';
 
 export default function PracticeTypeHome() {
   const { type } = useLocalSearchParams();
   const router = useRouter();
 
-  const option = PraticeOption.find(item => item.name == type);
+  const [loading, setLoading] = useState(false);
+  const option = PraticeOption.find(item => item.name === type);
+
+  const { userDetail } = useContext(UserDetailContext);
+
+  useEffect(() => {
+    if (userDetail) {
+      GetCourseList();
+    }
+  }, [userDetail]);
+
+  const GetCourseList = async () => {
+    try {
+      setLoading(true);
+
+      const q = query(
+        collection(db, 'Courses'),
+        where('createdBy', '==', userDetail?.email)
+      );
+
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      });
+
+    } catch (err) {
+      console.log("Error fetching courses:", err);
+    }
+
+    setLoading(false);
+  };
 
   return (
-    <View>
+    <View style={{ flex: 1, backgroundColor: Colors.WHITE }}>
+
+      {/* Background Image */}
       <Image 
-        source={option.image}
+        source={option?.image}
         style={{ height: 240, width: '100%' }}
+        resizeMode="cover"
       />
 
       {/* Overlay Section */}
@@ -42,6 +79,16 @@ export default function PracticeTypeHome() {
           {type}
         </Text>
       </View>
+
+      {/* Loader */}
+      {loading && (
+        <ActivityIndicator 
+          size="large" 
+          color={Colors.PRIMARY} 
+          style={{ marginTop: 150 }}
+        />
+      )}
+
     </View>
   );
 }
