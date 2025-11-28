b8172ac9afea4526e2b055a12504a74779981359
Pratik <pratikbavche2005@gmail.com>
2025-11-28 21:53:37 +0530

    flashcard section added

---
+++ app/flashcards/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FlashCards() {
+  return (
+    <View>
+      <Text>FlashCards</Text>
+    </View>
+  )
+}
--- components/PracticeScreen/CourseListGrid.jsx
+++ components/PracticeScreen/CourseListGrid.jsx
@@ -9,14 +9,12 @@ export default function CourseListGrid({ courseList, option }) {
   const router = useRouter();
 
   const onPress = (course) => {
-    if (option?.name === 'Quiz') {
       router.push({
-        pathname: '/quiz',
+        pathname: option.path,
         params: {
           courseParams: JSON.stringify(course)
         }
       });
-    }
   };
 
   return (
