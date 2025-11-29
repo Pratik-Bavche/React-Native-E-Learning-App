8eb6b26e067fddb7e86b2a4a35838ae1789953ad
Pratik <pratikbavche2005@gmail.com>
2025-11-29 21:40:33 +0530

    course list update

--- app/(tabs)/explore.jsx
+++ app/(tabs)/explore.jsx
@@ -2,9 +2,15 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function Explore() {
+
   return (
-    <View>
-      <Text>Explore</Text>
+    <View style={{
+      padding:25
+    }}>
+      <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize:20
+      }}>Explore More Courses</Text>
     </View>
   )
 }
